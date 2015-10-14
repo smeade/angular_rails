8c4c3f8f28b228ec66a66c55eb0ddda9f3f97344
Scott Meade <scott@synapsoftware.com>
2015-10-14 11:48:08 -0600

    Submitting Links

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ function($scope){
 
   $scope.addPost = function() {
     if(!$scope.title || $scope.title === '') {return};
-    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.posts.push({title: $scope.title, upvotes: 0, link: $scope.link});
     $scope.title = '';
+    $scope.link = '';
   };
 
   $scope.incrementUpvotes = function(post) {
