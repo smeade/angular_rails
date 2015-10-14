f5fb4aca2ecf31d526c99927d6125585764735c8
Scott Meade <scott@synapsoftware.com>
2015-10-14 11:42:39 -0600

    Enable Upvoting

--- app.js
+++ app.js
@@ -17,4 +17,8 @@ function($scope){
     $scope.posts.push({title: $scope.title, upvotes: 0});
     $scope.title = '';
   };
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes +=1 ;
+  };
 }]);
